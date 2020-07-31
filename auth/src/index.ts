de6542ae80e11174814a27de28e14d89776faed2
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-08-01 01:08:59 +0530

    Add mongoose to connect to mongoDB pod

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import { json } from 'body-parser';
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 import { NotFoundError } from './errors/not-found-error';
 import { errorHandler } from './middlewares/error-handler';
 import { currentUserRouter } from './routes/current-user';
@@ -23,4 +24,20 @@ app.all('*', async () => {
 // Set Middleware
 app.use(errorHandler);
 
-app.listen(3000, () => console.log('Server is up at 3000...'));
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => console.log('Server is up at 3000...'));
+};
+
+start();
