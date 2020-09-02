26097cc734e0a827afab7d18f1182e16aa960270
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-02 14:52:01 +0530

    Changes: - Add and Integrate cookie session support - Allow proxy for express

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
 import express from 'express';
 import 'express-async-errors';
 import mongoose from 'mongoose';
@@ -10,7 +11,15 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 
 const app = express();
+
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
