e853cdb3ef1802936a2af3ff2097fb2e8ef75ed9
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-04 14:43:03 +0530

    Add SignIn Logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { PasswordManager } from '../services/password';
 
 const router = express.Router();
 
@@ -16,6 +20,32 @@ router.post(
   validateRequest,
   async (req: Request, res: Response) => {
     const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email }).exec();
+    if (!existingUser) throw new BadRequestError('Invalid credentials!');
+
+    const passwordsMatch = await PasswordManager.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) throw new BadRequestError('Invalid credentials!');
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
   }
 );
 
