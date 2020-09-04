b956db2fd3884057b4bc58de0ad02ffc48a07643
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-04 14:25:07 +0530

    Update signin route: - add validation - add common req validation middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,22 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/singin', (req, res) => {
-  res.send('Hi there Signin');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+  }
+);
 
 export { router as signinRouter };
