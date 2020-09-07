9b98dd5bd6e6b7152101f7306693bf9659e2de36
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-07 13:15:47 +0530

    Create current user middleware

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.session?.jwt) return next();
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    req.currentUser = payload;
+  } catch (error) {}
+
+  next();
+};
