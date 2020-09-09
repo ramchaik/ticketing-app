1cd5dd81d82cdd9ee080a163c0409df68b7e61d8
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-09 17:17:17 +0530

    Add test to check if cookie is attahed

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -46,3 +46,12 @@ it('disallows duplicate emails', async () => {
     .send({ email: 'test@test.com', password: 'password' })
     .expect(400);
 });
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
