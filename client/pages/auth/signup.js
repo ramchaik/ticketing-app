421ef9854152ca0afac3991446535948dd0b9eef
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-14 23:53:24 +0530

    Handling Validations for Signup Component

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,16 +4,20 @@ import { useState } from 'react';
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
-
-    const response = await axios.post('/api/users/signup', {
-      email,
-      password,
-    });
-    console.log(response.data);
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password,
+      });
+    } catch (error) {
+      setErrors(error.response.data.errors);
+    }
   };
+
   return (
     <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
@@ -35,7 +39,16 @@ export default () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-
+      {errors.length > 0 && (
+        <div className='alert alert-danger'>
+          <h4>Ooops...</h4>
+          <ul className='my-0'>
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className='btn btn-primary'>Sign Up</button>
     </form>
   );
