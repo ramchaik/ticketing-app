f5f526861f619f52328f72b3cf3131eff3fd32dd
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-16 00:10:29 +0530

    Add onSuccess callback on use Request hook

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-export default ({ url, method, body }) => {
+export default ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
@@ -9,13 +9,16 @@ export default ({ url, method, body }) => {
       if (errors !== null) setErrors(null);
 
       const response = await axios[method](url, body);
+
+      if (onSuccess) onSuccess(response.data);
+
       return response.data;
     } catch (error) {
       setErrors(
         <div className='alert alert-danger'>
           <h4>Ooops...</h4>
           <ul className='my-0'>
-            {error.response.data.errors.map((e) => (
+            {(error.response.data.errors || []).map((e) => (
               <li key={e.message}>{e.message}</li>
             ))}
           </ul>
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,3 +1,4 @@
+import Router from 'next/router';
 import { useState } from 'react';
 import useRequest from '../../hooks/use-request';
 
@@ -8,11 +9,12 @@ export default () => {
     method: 'post',
     url: '/api/users/signup',
     body: { email, password },
+    onSuccess: () => Router.push('/'),
   });
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    doRequest();
+    await doRequest();
   };
 
   return (
