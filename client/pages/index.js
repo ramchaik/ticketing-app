49b6ae7dfe571a12a8240856ce15ea486b5c904a
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-22 19:52:54 +0530

    Add logic to make current user request from: - browser - server (nextJS)

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,33 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log('LandingPage -> currentUser', currentUser);
+
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // On Server
+    // Base URL to hit a service from server (inside container) - http://SERVICE_NAME.NAMESPACE.svc.cluster.local
+    // Need to add header of Req, so we can set cookie
+
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    // On Browser
+    // base url handled by browser, made same as domain on browser
+
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+};
+
+export default LandingPage;
