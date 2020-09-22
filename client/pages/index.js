37f6e39d07b5772ae620c9e339a4a34b28ea930f
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-22 20:15:10 +0530

    Check current user

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,9 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log('LandingPage -> currentUser', currentUser);
-
-  return <h1>Landing Page</h1>;
+  return (
+    <h1>{currentUser ? 'YOU are signed in!!' : 'YOU are NOT signed in!!'}</h1>
+  );
 };
 
 LandingPage.getInitialProps = async (context) => {
