4f3c1232c52c2ff8a801292f1c5afd729b65bc6d
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-22 21:25:30 +0530

    Abstract logic to App Component: - Add getInitialProps on AppComponent - Add manual call of getInitialProps to components being rendered by App Components

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,3 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps }) => <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => (
+  <div>
+    <h1>Header</h1>
+    <Component {...pageProps} />
+  </div>
+);
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  console.log('AppComponent.getInitialProps -> pageProps', pageProps);
+
+  return data;
+};
+
+export default AppComponent;
