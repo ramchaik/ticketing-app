36ea7455296105454c203685b9091d5a4e20bda5
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-22 20:09:17 +0530

    Update Landing page with buildClient API

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log('LandingPage -> currentUser', currentUser);
@@ -6,28 +6,11 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // On Server
-    // Base URL to hit a service from server (inside container) - http://SERVICE_NAME.NAMESPACE.svc.cluster.local
-    // Need to add header of Req, so we can set cookie
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
-
-    return data;
-  } else {
-    // On Browser
-    // base url handled by browser, made same as domain on browser
-
-    const { data } = await axios.get('/api/users/currentuser');
-
-    return data;
-  }
+  return data;
 };
 
 export default LandingPage;
