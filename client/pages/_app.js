827275fd354e2def2ad6160a595404b165935a0a
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-24 17:33:36 +0530

    Add Header

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-const AppComponent = ({ Component, pageProps }) => (
+const AppComponent = ({ Component, pageProps, currentUser }) => (
   <div>
-    <h1>Header</h1>
+    <Header currentUser={currentUser} />
     <Component {...pageProps} />
   </div>
 );
@@ -16,9 +17,11 @@ AppComponent.getInitialProps = async (appContext) => {
   if (appContext.Component.getInitialProps) {
     pageProps = await appContext.Component.getInitialProps(appContext.ctx);
   }
-  console.log('AppComponent.getInitialProps -> pageProps', pageProps);
 
-  return data;
+  return {
+    pageProps,
+    ...data,
+  };
 };
 
 export default AppComponent;
