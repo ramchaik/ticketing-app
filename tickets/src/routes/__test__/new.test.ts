3e20f6bd79c4009a44339eb1161fb3b5505f1b6b
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-29 13:56:22 +0530

    Add test cases to check price and title on post req

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -21,11 +21,41 @@ it('returns a status other than 401 if the user is signed in', async () => {
 });
 
 it('returns an error if an invalid title is provided', async () => {
-  request(app);
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
 });
 
 it('returns an error if an invalid price is provided', async () => {
-  request(app);
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'aslkdfj',
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      tickets: 'alskdfjasdfj',
+    })
+    .expect(400);
 });
 
 it('creates a ticket with valid inputs', async () => {
