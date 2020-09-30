f80a7ee1dccf19dd7d7cd17db47c09144e716252
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-30 11:04:10 +0530

    Change Logs: - Add tickets schema - Add static method to build document - Add model

--- tickets/src/models/tickets.ts
+++ tickets/src/models/tickets.ts
@@ -12,6 +12,39 @@ interface TicketDoc extends mongoose.Document {
   userId: string;
 }
 
-interface TikcktModel extends mongoose.Model<TicketDoc> {
+interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
 }
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket };
