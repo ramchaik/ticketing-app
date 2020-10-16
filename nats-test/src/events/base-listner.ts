12ebc7b7e46ec010cd48c237602f4b4c6e196b30
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-10-16 11:28:28 +0530

    Update Listner class to a generic class of Event interface

--- nats-test/src/events/base-listner.ts
+++ nats-test/src/events/base-listner.ts
@@ -1,9 +1,14 @@
 import { Message, Stan } from 'node-nats-streaming';
+import { Subjects } from './subjects';
 
-export abstract class Listner {
-  abstract subject: string;
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+export abstract class Listner<T extends Event> {
+  abstract subject: T['subject'];
   abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
+  abstract onMessage(data: T['data'], msg: Message): void;
   private client: Stan;
   protected ackWait = 5 * 1000;
 
