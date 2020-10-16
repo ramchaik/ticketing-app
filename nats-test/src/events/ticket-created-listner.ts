374198fe9eb7ee10fb6c3a8c5cdc307c80e916de
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-10-16 23:15:46 +0530

    Add type check for data on message

--- nats-test/src/events/ticket-created-listner.ts
+++ nats-test/src/events/ticket-created-listner.ts
@@ -7,7 +7,7 @@ export class TicketCreatedListner extends Listner<TicketCreatedEvent> {
   readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log(`Event Data:`, data);
 
     msg.ack();
