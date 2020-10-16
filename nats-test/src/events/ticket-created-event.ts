972c0fdc8b6c11fd66f371621a4b38f6ad3de7b3
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-10-16 10:58:34 +0530

    Add Custom Event Interface

---
+++ nats-test/src/events/ticket-created-event.ts
@@ -0,0 +1,11 @@
+import { Subjects } from './subjects';
+
+export interface TicketCreatedEvent {
+  subject: Subjects.TicketCreated;
+  data: {
+    id: string;
+    name: string;
+    price: number;
+    
+  }
+}
