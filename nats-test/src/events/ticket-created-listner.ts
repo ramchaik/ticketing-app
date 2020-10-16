3045d27883c9d962a338baa206917302c01c365d
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-10-16 11:29:43 +0530

    Update Listner to extends Generic class
    To avoid any typo in subject of the event and to add type checking on event

--- nats-test/src/events/ticket-created-listner.ts
+++ nats-test/src/events/ticket-created-listner.ts
@@ -1,8 +1,10 @@
 import { Message } from 'node-nats-streaming';
 import { Listner } from './base-listner';
+import { Subjects } from './subjects';
+import { TicketCreatedEvent } from './ticket-created-event';
 
-export class TicketCreatedListner extends Listner {
-  subject = 'ticket:created';
+export class TicketCreatedListner extends Listner<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
   onMessage(data: any, msg: Message) {
