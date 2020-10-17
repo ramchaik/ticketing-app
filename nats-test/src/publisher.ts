a0feb662913e3f98da83be2c67bf05394dfe6a88
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-10-17 23:21:01 +0530

    Awaiting Event Publication

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -4,17 +4,21 @@ import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 console.clear();
 
 const stan = nats.connect('ticketing', 'abc', {
-  url: 'http://localhost:4222'
+  url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
-  console.log("Publisher connected to NATS");
+stan.on('connect', async () => {
+  console.log('Publisher connected to NATS');
 
   const publisher = new TicketCreatedPublisher(stan);
 
-  publisher.publish({
-    id: '123',
-    title: 'concert',
-    price: 20
-  });
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 20,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
