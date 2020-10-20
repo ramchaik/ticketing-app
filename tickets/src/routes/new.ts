329959beb04db27f75c11edbaee8a20b0832f07f
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-10-20 23:04:41 +0530

    Accessing nats client

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,7 +1,9 @@
 import { requireAuth, validateRequest } from '@vsrtickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
 import { Ticket } from '../models/tickets';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -20,6 +22,13 @@ router.post(
 
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
     await ticket.save();
+
+    new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId
+    });
 
     res.status(201).send(ticket);
   }
