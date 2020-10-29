27a3a973b4ea469577cd7d8bce4463c6ee950158
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-10-29 10:56:07 +0530

    Add orders service's route handlers

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,10 +3,10 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
 import 'express-async-errors';
-import { indexTicketRouter } from './routes';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { updateTicketRouter } from './routes/update';
+import { indexOrderRouter } from './routes';
+import { deleteOrderRouter } from './routes/delete';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
 
@@ -18,12 +18,13 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
