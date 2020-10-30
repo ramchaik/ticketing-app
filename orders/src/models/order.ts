f588426b25a3fc517fd8426e4806f47b8189cbbd
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-10-30 14:12:30 +0530

    Defining the Ticket model

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,5 +1,6 @@
-import mongoose from 'mongoose';
 import { OrderStatus } from '@vsrtickets/common';
+import mongoose from 'mongoose';
+import { TicketDoc } from './ticket';
 
 interface OrderAttrs {
   userId: string;
