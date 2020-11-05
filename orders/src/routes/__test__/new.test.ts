e209d0decfb3401bd9bd27de2d5b474391f4f641
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-11-05 21:24:28 +0530

    Asserting reserved ticket

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,15 +1,43 @@
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
-import mongoose from 'mongoose';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
 
 it('returns an error if the ticket does not exist', async () => {
   const ticketId = mongoose.Types.ObjectId();
 
-  await request(app).post('/api/orders').set('Cookie', global.signin()).send({
-    ticketId
-  }).expect(404);
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({
+      ticketId,
+    })
+    .expect(404);
 });
 
-it('returns an error if the ticket is already reserved', async () => {});
+it('returns an error if the ticket is already reserved', async () => {
+  const ticket = await Ticket.build({
+    title: 'A',
+    price: 3,
+  });
+  await ticket.save();
+
+  const order = await Order.build({
+    ticket,
+    userId: 'alskdfjlaksjdf',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(400);
+});
 
 it('reserves a ticket', async () => {});
