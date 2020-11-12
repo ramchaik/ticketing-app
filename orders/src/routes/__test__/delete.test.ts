68b4e6922be58f4319d0e836b7404b5ba28ed22f
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-11-12 12:38:52 +0530

    Test cancel order

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,35 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
+
+it('marks an order as cancelled', async () => {
+  // create a ticket with Ticket model
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 30,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // Make a request to create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+
+  // Make a request to cancel an order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  // Expect to make sure the order is cancelled
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
