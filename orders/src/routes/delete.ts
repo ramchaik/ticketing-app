3d6cd49fecad1e27fdbd4400a9659f36bd49e197
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-11-12 16:58:15 +0530

    Add call to publish, cancel order event

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,29 +1,44 @@
-import { NotAuthorizedError, NotFoundError, requireAuth } from '@vsrtickets/common';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@vsrtickets/common';
 import express, { Request, Response } from 'express';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId',requireAuth, async (req: Request, res: Response) => {
-  const { orderId } = req.params;
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
 
-  const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
-  if (!order) {
-    throw new NotFoundError();
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order?.userId !== req.currentUser?.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // publishing an event saying this was cancelled!
+    await new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    res.status(204).send(order);
   }
-
-  if (order?.userId !== req.currentUser?.id) {
-    throw new NotAuthorizedError();
-  }
-
-  order.status = OrderStatus.Cancelled;
-  await order.save();
-
-  // publishing an event saying this was cancelled!
-
-
-  res.status(204).send(order);
-});
+);
 
 export { router as deleteOrderRouter };
