728270e24b214e5f1efe1e6d82cdd7dbc67e1d1a
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-11-18 14:01:44 +0530

    Rename file

---
+++ tickets/src/models/__test__/tickets.test.ts
@@ -0,0 +1,33 @@
+import { Ticket } from '../tickets';
+
+it('implements optimistic concurrency control', async (done) => {
+  // Create an instance of ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 30,
+    userId: '123',
+  });
+
+  // Save the ticket to the database
+  await ticket.save();
+
+  // Fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // Make 2 separate changes to the tickets we fetched
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 100 });
+
+  // Save the first fetched ticket
+  await firstInstance!.save();
+
+  // Save the second fetched ticket and expect an error
+  try {
+    await secondInstance!.save();
+  } catch (error) {
+    return done();
+  }
+
+  throw new Error('Should not reach this point');
+});
