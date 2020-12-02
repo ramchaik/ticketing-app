407c4a566b2f6f9f1f5db1eae3b81becfa71380b
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-12-02 21:20:54 +0530

    Add tests for order cancelled listener

---
+++ tickets/src/events/listners/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,68 @@
+import { OrderCancelledEvent } from '@vsrtickets/common';
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../../models/ticket';
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCancelledListener } from '../order-cancelled-listener';
+
+const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const orderId = mongoose.Types.ObjectId().toHexString();
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 30,
+    userId: 'abc',
+  });
+
+  ticket.set({ orderId });
+
+  await ticket.save();
+
+  const data: OrderCancelledEvent['data'] = {
+    id: orderId,
+    version: 0,
+    ticket: {
+      id: ticket.id,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, orderId, data, msg };
+};
+
+it('updates the ticket', async () => {
+  const { listener, data, ticket, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket?.orderId).not.toBeDefined();
+});
+
+it('publishes the event', async () => {
+  const { listener, data, ticket, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  const updatedTicket = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+
+  expect(ticket.id).toEqual(updatedTicket.id);
+});
+
+it('acks the message', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
