fe7081d665f8e5e0bf0fff3bb5db228cbcb3c880
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-12-08 08:32:36 +0530

    Rejecting Edits of Reserved tickets

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,5 +1,7 @@
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 import { natsWrapper } from '../../nats-wrapper';
 import { getRandomIdHexString } from './utils/genericUtils';
 
@@ -85,10 +87,10 @@ it('updates the ticket provided valid inputs', async () => {
       title: 'alskdf',
       price: 32,
     });
-  
+
   const updatedTicket = {
-      title: 'Hello',
-      price: 30,
+    title: 'Hello',
+    price: 30,
   };
 
   await request(app)
@@ -96,8 +98,10 @@ it('updates the ticket provided valid inputs', async () => {
     .set('Cookie', cookie)
     .send(updatedTicket)
     .expect(200);
-  
-  const ticketResponse = await request(app).get(`/api/tickets/${response.body.id}`).send({});
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send({});
 
   expect(ticketResponse.body.title).toEqual(updatedTicket.title);
   expect(ticketResponse.body.price).toEqual(updatedTicket.price);
@@ -113,10 +117,10 @@ it('publish an event', async () => {
       title: 'alskdf',
       price: 32,
     });
-  
+
   const updatedTicket = {
-      title: 'Hello',
-      price: 30,
+    title: 'Hello',
+    price: 30,
   };
 
   await request(app)
@@ -124,6 +128,34 @@ it('publish an event', async () => {
     .set('Cookie', cookie)
     .send(updatedTicket)
     .expect(200);
-  
+
   expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('rejects update if ticket is reserved ', async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post(`/api/tickets`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'alskdf',
+      price: 32,
+    });
+
+  const ticket = await Ticket.findById(response.body.id);
+
+  ticket!.set({ orderId: mongoose.Types.ObjectId().toHexString() });
+  ticket!.save();
+
+  const updatedTicket = {
+    title: 'Hello',
+    price: 30,
+  };
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send(updatedTicket)
+    .expect(400);
 });
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestError,
   NotAuthorizedError,
   NotFoundError,
   requireAuth,
@@ -33,6 +34,10 @@ router.put(
 
     if (ticket.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError('Cannot edit a reserved ticket.');
     }
 
     ticket.set({
