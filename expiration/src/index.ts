1de34b37751669d176bdbf7d316ebf14ef2995ae
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-12-09 09:19:19 +0530

    Remove log

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -21,8 +21,6 @@ const start = async () => {
 
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
-
-    console.log('Connected to MongoDB');
   } catch (error) {
     console.error(error);
   }
