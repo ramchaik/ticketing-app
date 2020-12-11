143d69b599e87cfe583b9db9e528ed22367b5e89
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-12-11 10:25:35 +0530

    Publish expiration complete event when job is processed

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,4 +1,6 @@
 import Queue from 'bull';
+import { ExpirationCompletePublisher } from '../events/publishers/expiration-complete-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 interface Payload {
   orderId: string;
@@ -11,8 +13,11 @@ const expirationQueue = new Queue<Payload>('order:expiration', {
 });
 
 expirationQueue.process(async (job) => {
-  const { orderId } = job.data;
-  console.log('PUBLISH expiration:complete event for orderId ->', orderId);
+  const { orderId } = job.data || {};
+
+  new ExpirationCompletePublisher(natsWrapper.client).publish({
+    orderId,
+  });
 });
 
 export { expirationQueue };
