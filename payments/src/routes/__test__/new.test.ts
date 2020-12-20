215b133068e2d38852305b64110fd90474e2cb69
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-12-20 10:34:34 +0530

    test(payments): add test for order validation before payment

---
+++ payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,38 @@
+import { OrderStatus } from '@vsrtickets/common';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+
+it('returns a 404 when purchasing an order that does not exit', async () => {
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin())
+    .send({
+      token: 'alksjdf',
+      orderId: mongoose.Types.ObjectId(),
+    })
+    .expect(404);
+});
+
+it("returns a 401 when purchasing an order that doesn't belong to the user", async () => {
+  const order = await Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    userId: mongoose.Types.ObjectId().toHexString(),
+    price: 10,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin())
+    .send({
+      token: 'alksjdf',
+      orderId: order.id,
+    })
+    .expect(40);
+});
+
+it('returns a 400 when purchasing a cancelled order', async () => {});
