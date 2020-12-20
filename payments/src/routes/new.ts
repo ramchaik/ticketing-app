f643d8d864cacaac84d2099e194d310ea5a3ddbf
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-12-20 13:44:32 +0530

    feat(payments): add create charge with stripe

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -27,6 +28,12 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for a cancelled order');
     }
+
+    await stripe.charges.create({
+      currency: 'inr',
+      amount: order.price * 100,
+      source: token,
+    });
 
     res.send({ success: true });
   }
