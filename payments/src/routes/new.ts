f9e0082621ca820315c64cf435e6157d6b422911
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-12-21 21:43:40 +0530

    feat(payments): publish create payment event

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -8,8 +8,10 @@ import {
 } from '@vsrtickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
 import { Order } from '../models/order';
 import { Payment } from '../models/payment';
+import { natsWrapper } from '../nats-wrapper';
 import { stripe } from '../stripe';
 
 const router = express.Router();
@@ -42,7 +44,13 @@ router.post(
     });
     await payment.save();
 
-    res.status(201).send({ success: true });
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id!,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
+
+    res.status(201).send({ id: payment.id });
   }
 );
 
