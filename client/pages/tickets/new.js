be4c419530b23ecda35cc71962b52379bac08a79
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-12-23 21:50:31 +0530

    feat(client/ticket): sanitize price input and add state to form

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NewTicket = () => {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+
+    if (isNaN(value)) return;
+
+    setPrice(value.toFixed(2));
+  };
+
   return (
     <div>
       <h1>Create a Ticket</h1>
       <form>
         <div className='form-group'>
           <label>Title</label>
-          <input type='text' className='form-control' />
+          <input
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='form-control'
+          />
         </div>
         <div className='form-group'>
           <label>Price</label>
-          <input type='text' className='form-control' />
+          <input
+            type='text'
+            value={price}
+            onBlur={onBlur}
+            onChange={(e) => setPrice(e.target.value)}
+            className='form-control'
+          />
         </div>
         <button className='btn btn-primary'>Submit</button>
       </form>
