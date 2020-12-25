deb7c13b245f2b941fd818c662eda205115f38e3
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-12-25 20:19:06 +0530

    feat(client): add redirection after ticket creation to home

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,3 +1,4 @@
+import Router from 'next/router';
 import React, { useState } from 'react';
 import useRequest from '../../hooks/use-request';
 
@@ -11,7 +12,7 @@ const NewTicket = () => {
       title,
       price,
     },
-    onSuccess: (ticket) => console.log(ticket),
+    onSuccess: () => Router.push('/'),
   });
 
   const handleSubmit = (event) => {
